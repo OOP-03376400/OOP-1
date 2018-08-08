cadefd2973f706cede53b22dd2f27b939021761c
skeletonbrrr <ri.tysik@mail.ru>
2018-08-08 16:25:39 +0300

    Classes and objects

---
+++ labrab3/labrab3/Class1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labrab3
+{
+    public partial class Set
+    {
+        public int[] massif = new int[maxsize];
+        static int maxsize = 30;
+        public int size;
+        public static int st = 0;
+
+        public Set() //без параметров
+        {
+            size = 0;
+            ++st;
+        }
+
+        public Set(int[] massif, int size) //с параметрами
+        {
+            this.massif = massif;
+            this.size = size;
+            ++st;
+            inf();
+        }
+
+        public Set(int size = 0) //с параметром по умолчанию
+        {
+            this.size = size;
+            ++st;
+        }
+
+        static Set() //статический конструктор (вызывается автоматически перед созданием первого экземпляра)
+                     //нельзя вызвать напрямую, выполняется один раз
+        {
+
+        }
+
+        private Set(int[] massif) //Закрытый конструктор — это особый конструктор экземпляров. 
+                                  //Обычно он используется в классах, содержащих только статические элементы.
+                                  //Если в классе один или несколько закрытых конструкторов и ни одного открытого конструктора, 
+                                  //то прочие классы (за исключением вложенных классов) не смогут создавать экземпляры этого класса. 
+        {
+            this.massif = massif;
+        }
+
+        public readonly int id = 23; //поле только для чтения
+
+        const int constt = 160298; //поле- константа
+
+        public int SetGet //объявление свойства
+        {
+            get // аксессор чтения поля
+            {
+                return massif[size];
+            }
+            set // аксессор записи в поле
+            {
+                if (size == 0)
+                {
+                    Console.WriteLine("Пустое множество ");
+                }
+                else
+                {
+                    massif[size] = value;
+                    size++;
+                }
+                    
+            }
+        }
+
+        public int Get //ограничение по set
+        {
+            get
+            {
+                return massif[size];
+            }
+        }
+
+        public void sqr(ref int a) //вызов по ссылке для нессылочных типов
+        {
+            a = a * a;
+        }
+
+        public void sum (int b, out int m) //обязательно присвоить значение. исп для передачи значения из метода
+        {
+            int c = b + 1;
+            m = c;
+        }
+
+       
+        public void inf()
+        {
+            Console.WriteLine("В классе содержится " + st + " объектов");
+        }
+
+        public override bool Equals(Object obj)
+        {
+            bool result = false;
+            if (obj == null || !(obj is Set))
+                return false;
+            else
+                if (size == ((Set)obj).size)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (massif[i]== ((Set)obj).massif[i])
+                    {
+                        result = true;
+                    }
+                }
+            }
+            if (!result)
+                return false;
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 269;
+            if (size == 0)
+            {
+                hash = 0;
+            }
+            else for (int i = 0; i < size; i++)
+                {
+                    hash = massif[i].GetHashCode();
+                }
+            hash = hash * 47;
+            return hash;
+        }
+
+
+        public override string ToString()
+        {
+            string inf = "";
+            for (int i = 0; i < size; i++)
+            {
+                inf = inf + massif[i].ToString() + " ";
+            }
+            return "Type " + base.ToString() + " Множество: " + inf;
+        }
+    }
+
+}
+
---
+++ labrab3/labrab3/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labrab3
+{
+    public partial class Set
+    {
+        public void Add(int element)
+        {
+            massif[size] = element;
+            size++;
+        }
+
+        public void Remove(int element)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (massif[j] == element)
+                {
+                    for(int n = j; n < size; n++)
+                    {
+                        massif[n] = massif[n + 1];
+                    }
+                }
+            }
+            size--;
+        }
+
+        public void Amount()
+        {
+            Console.WriteLine("Текущее количество элементов: " + size);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Множество: ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(massif[i]);
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Set set1 = new Set();
+            Set set2 = new Set();
+            Set set3 = new Set();
+
+            Console.WriteLine("Количество созданных объектов: ");
+            set3.inf();
+            Console.WriteLine();
+
+            Console.WriteLine("Множество 1");
+            set1.Add(2);
+            set1.Add(-11);
+            set1.Add(5);
+            set1.Remove(2);
+            set1.Amount();
+            set1.Show();
+            Console.WriteLine();
+
+            Console.WriteLine("Измененное с помощью свойства множество 1");
+            set1.SetGet = 3;
+            set1.Show();
+            Console.WriteLine();
+
+            set1.Remove(3);
+
+            Console.WriteLine("Множество 2");
+            set2.Add(-11);
+            set2.Add(5);
+            set2.Amount();
+            set2.Show();
+            Console.WriteLine();
+
+            Console.WriteLine("Хэш первого множества " + set1.GetHashCode());
+            Console.WriteLine("Хэш второго множества " + set2.GetHashCode());
+            Console.WriteLine();
+
+            Console.WriteLine("Множество 3");
+            set3.Add(-2);
+            set3.Add(5);
+            set3.Add(8);
+            set3.Amount();
+            set3.Show();
+            Console.WriteLine(); 
+
+            Console.WriteLine("Тип объекта set3: " + set3.GetType());
+            Console.WriteLine();
+            Console.WriteLine("Информация об объекте set3: " + set3.ToString());
+            Console.WriteLine();
+            Console.WriteLine("is set1 == set2? " + set2.Equals(set1));
+            Console.WriteLine();
+
+
+            List<Set> sete = new List<Set>();
+            sete.Add(set1);
+            sete.Add(set2);
+            sete.Add(set3);
+            sete.Add(new Set());
+            Console.WriteLine();
+
+
+
+            int max = -1000;
+            int min = 1000;
+            int sum = 0;
+            foreach (var k in sete)
+            {
+                for (int i = 0; i < k.size; i++)
+                {
+                    sum += k.massif[i];
+                }
+
+                if (sum < min)
+                {
+                    min = sum;
+                }
+                if (sum > max)
+                {
+                    max = sum;
+                }
+                sum = 0;
+            }
+
+            foreach (var k in sete)
+            {
+                for (int i = 0; i < k.size; i++)
+                {
+                    sum += k.massif[i];
+                }
+
+                if (sum == min)
+                {
+                    string inf = "";
+                    for (int i = 0; i < k.size; i++)
+                    {
+                        inf = inf + k.massif[i].ToString() + " ";
+                    }
+                    Console.WriteLine("Множество с наименьшей суммой элементов: " + inf + "Сумма: " + min);
+                }
+                if (sum == max)
+                {
+                    string inf = "";
+                    for (int i = 0; i < k.size; i++)
+                    {
+                        inf = inf + k.massif[i].ToString() + " ";
+                    }
+                    Console.WriteLine("Множество с наибольшей суммой элементов: " + inf + "Сумма: " + max);
+                }
+                sum = 0;
+            }
+
+            Console.WriteLine();
+            int nul = 0;
+            bool tr = false;
+            Console.WriteLine("Список множеств, содержащих отрицательные элементы: ");
+            foreach (var k in sete)
+            {
+                for (int i = 0; i < k.size; i++)
+                {
+                    if (k.massif[i] < 0)
+                    {
+                        tr = true;
+                    }
+                }
+                if (tr)
+                {
+                    k.Show();
+                    nul++;
+                }
+                tr = false;
+            }
+            if (nul == 0)
+            {
+                Console.WriteLine("Такие множества отсутствуют");
+            }
+
+
+            var AnonymType = new { mass = new[] { 1, 2, 3 } };
+}
+    }
+}
+
+
+
