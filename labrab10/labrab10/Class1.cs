9c1d562ef3553d76f88552c87cf391ca7ae67a30
skeletonbrrr <ri.tysik@mail.ru>
2018-08-08 19:35:29 +0300

    Collections

---
+++ labrab10/labrab10/Class1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace labrab10
+{
+    public class CountIsZeroException : Exception
+    {
+        public CountIsZeroException()
+        {
+        }
+        public CountIsZeroException(string message)
+        : base(message)
+        {
+        }
+    }
+
+    public class WrongValue : Exception
+    {
+        public WrongValue()
+        {
+        }
+        public WrongValue(string message)
+        : base(message)
+        {
+        }
+    }
+
+    public class WrongAmount : Exception
+    {
+        public WrongAmount()
+        {
+        }
+        public WrongAmount(string message)
+        : base(message)
+        {
+        }
+    }
+
+
+
+
+    public abstract class Товар : Interface
+    {
+        public string name = "";
+        public string producer = "";
+        public int price;
+        public int amount;
+        public abstract void ShowInformation();
+
+        public virtual void InputInformation()
+        {
+
+            if (this.name == "Мебель")
+            {
+                Console.WriteLine($" Введите информацию о  {name}:");
+                Console.Write("Количество: ");
+                amount = int.Parse(Console.ReadLine());
+                if (amount < 0)
+                {
+                    throw (new WrongAmount("Количество не может быть меньше нуля"));
+                }
+                Console.Write("Производитель: ");
+                producer = Console.ReadLine();
+                if (producer.Length < 2)
+                {
+                    throw (new WrongValue("Cлишком короткое имя"));
+                }
+                Debug.Assert(producer.Length < 10, "Длина превышает 10 символов");
+            }
+        }
+        public virtual void FindingPrice()
+        {
+            if (name == "Мебель")
+            {
+                if (amount == 0)
+                    throw (new CountIsZeroException("Нельзя делить на ноль"));
+                else
+                    price = 23654 / amount;
+            }
+            Console.WriteLine("Нахождение цены");
+            Console.WriteLine($"Цена {name} : ");
+        }
+    }
+    class Мебель : Товар
+    {
+        public Мебель()
+        {
+            name = "Мебель";
+        }
+        public override void ShowInformation()
+        {
+            Console.WriteLine("Вывод информации ");
+            Console.WriteLine($"Товар {name}");
+
+            Console.WriteLine($"Цена {name} = {price}");
+            Console.WriteLine();
+
+        }
+        public override void InputInformation()
+        {
+            if (this.name == "Мебель")
+            {
+                Console.WriteLine($" Введите информацию о  {name}:");
+                Console.Write("Количество: ");
+                amount = int.Parse(Console.ReadLine());
+                if (amount < 0)
+                {
+                    throw (new WrongAmount("Количество не может быть меньше нуля"));
+                }
+                Console.Write("Производитель: ");
+                producer = Console.ReadLine();
+                if (producer.Length < 2)
+                {
+                    throw (new WrongValue("Cлишком короткое имя"));
+                }
+                Debug.Assert(producer.Length < 10, "Длина превышает 10 символов");
+            }
+        }
+        public override void FindingPrice()
+        {
+            if (name == "Мебель")
+            {
+                if (amount == 0)
+                    throw (new CountIsZeroException("Нельзя делить на ноль"));
+                else
+                    price = 23654 / amount;
+            }
+            Console.WriteLine("Нахождение цены");
+            Console.WriteLine($"Цена {name} : {price}");
+        }
+        public override string ToString()
+        {
+            string str = "";
+            Console.WriteLine("Тип объекта 'Мебель'");
+            Console.WriteLine($"Информация о: {name} \n Количество = {amount} \n Производитель = {producer} \n Цена = {price}");
+            return str;
+        }
+    }
+    class Диван : Мебель
+    {
+        public override string ToString()
+        {
+            string str = "";
+            Console.WriteLine("Тип объекта 'Диван'");
+            Console.WriteLine($"Информация о: {name} \n Количество = {amount} \n Производитель = {producer} \n Цена = {price}");
+            return str;
+        }
+        public Диван()
+        {
+            name = "Диван";
+        }
+        public override void ShowInformation()
+        {
+            Console.WriteLine("Вывод информации ");
+            Console.WriteLine($"Мебель {name}");
+
+            Console.WriteLine($"Цена {name} = {price}");
+            Console.WriteLine();
+
+        }
+        public override void InputInformation()
+        {
+            if (this.name == "Диван")
+            {
+                Console.WriteLine($" Введите информацию о  {name}:");
+                Console.Write("Количество: ");
+                amount = int.Parse(Console.ReadLine());
+                if (amount < 0)
+                {
+                    throw (new WrongAmount("Количество не может быть меньше нуля"));
+                }
+                Console.Write("Производитель: ");
+                producer = Console.ReadLine();
+                if (producer.Length < 2)
+                {
+                    throw (new WrongValue("Cлишком короткое имя"));
+                }
+                Debug.Assert(producer.Length < 10, "Длина превышает 10 символов");
+            }
+        }
+        public override void FindingPrice()
+        {
+            if (name == "Диван")
+            {
+                if (amount == 0)
+                    throw (new CountIsZeroException("Нельзя делить на ноль"));
+                else
+                    price = 23654 / amount;
+            }
+            Console.WriteLine("Нахождение цены ");
+            Console.WriteLine($"Цена {name} : {price}");
+        }
+
+    }
+    class Кровать : Мебель
+    {
+        public override string ToString()
+        {
+            string str = "";
+            Console.WriteLine("Тип объекта 'Кровать'");
+            Console.WriteLine($"Информация о: {name} \n Количество = {amount} \n Производитель = {producer} \n Цена = {price}");
+            return str;
+        }
+        public Кровать()
+        {
+            name = "Кровать";
+        }
+        public override void ShowInformation()
+        {
+            Console.WriteLine("Вывод информации");
+            Console.WriteLine($"Мебель {name}");
+
+            Console.WriteLine($"Цена {name} = {price}");
+            Console.WriteLine();
+
+        }
+        public override void InputInformation()
+        {
+            if (this.name == "Кровать")
+            {
+                Console.WriteLine($" Введите информацию о  {name}:");
+                Console.Write("Количество: ");
+                amount = int.Parse(Console.ReadLine());
+                if (amount < 0)
+                {
+                    throw (new WrongAmount("Количество не может быть меньше нуля"));
+                }
+                Console.Write("Производитель: ");
+                producer = Console.ReadLine();
+                if (producer.Length < 2)
+                {
+                    throw (new WrongValue("Cлишком короткое имя"));
+                }
+                Debug.Assert(producer.Length < 10, "Длина превышает 10 символов");
+            }
+        }
+        public override void FindingPrice()
+        {
+            if (name == "Кровать")
+            {
+                if (amount == 0)
+                    throw (new CountIsZeroException("Нельзя делить на ноль"));
+                else
+                    price = 23654 / amount;
+            }
+            Console.WriteLine("Нахождение цены");
+            Console.WriteLine($"Цена {name} : {price}");
+        }
+    }
+    interface Interface
+    {
+        void ShowInformation();
+        void InputInformation();
+        void FindingPrice();
+    }
+
+
+    //sealed class A
+    //{
+
+    //}
+    //class B : A
+    //{
+
+    //}
+
+    class Printer : Товар
+    {
+        public override void ShowInformation()
+        {
+            Console.WriteLine("Вывод информации ");
+            Console.WriteLine($"Товар {name}");
+
+            Console.WriteLine($"Цена {name} = {price}");
+            Console.WriteLine(); ;
+        }
+        public virtual void iAmPrinting(Товар someobj)
+        {
+            Console.WriteLine(someobj.GetType());
+            someobj.ToString();
+        }
+    }
+}
---
+++ labrab10/labrab10/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+
+namespace labrab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            ///////////////////////////////////////////////////////////////////////////////
+            ArrayList list = new ArrayList();
+            Random цифра = new Random();
+            int[] a = new int[6];
+            for (int i = 0; i < 5; i++)
+            {
+                a[i] = цифра.Next(0, 100);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(a[i]);
+            }
+            foreach (object ind in list)
+            {
+                Console.WriteLine(ind);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            list.AddRange(new string[] { "hello wrold !!!" });
+            foreach (object ind in list)
+            {
+                Console.WriteLine(ind);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            list.RemoveAt(3);
+            foreach (object ind in list)
+            {
+                Console.WriteLine(ind);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine(list.Count);
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine(list.Contains("hello wrold !!!"));
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+
+            //Задание 2
+            ///////////////////////////////////////////////////////////////////////////////
+            HashSet<long> Q = new HashSet<long>();
+            Console.WriteLine("Введите количество элементов.");
+            int per = int.Parse(Console.ReadLine());
+            Q.Add(1);
+            for (int i = 0; i < per; i++)
+            {
+                Console.WriteLine("Введите элемент.");
+                long ch = long.Parse(Console.ReadLine());
+                Q.Add(ch);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("HashSet: ");
+            foreach (object b in Q)
+            {
+                Console.WriteLine(b);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("Введите количество эдементов для удаления ");
+            int col = int.Parse(Console.ReadLine());
+            long[] arr = new long[col];
+            if (col > Q.Count())
+            {
+                Console.WriteLine("Превышение размера множества ");
+            }
+            else
+            {
+
+                Console.WriteLine("Введите эдементы для удаления ");
+                for (int i=0; i < col; i++)
+                {
+                    arr[i] = long.Parse(Console.ReadLine());
+                }
+                for (int i = 0; i < col; i++)
+                {
+                   Q.Remove(arr[i]);
+                }
+            }
+            Console.WriteLine();
+            foreach (object b in Q)
+            {
+                Console.WriteLine(b);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("Введите элемент.");
+            long che = long.Parse(Console.ReadLine());
+            Q.Add(che);
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            int size = Q.Count();
+            LinkedList<long> d = new LinkedList<long>();
+            long[] buf = new long[size];
+            Q.CopyTo(buf);
+            d.AddFirst(buf[0]);
+            for (int i = 1; i < size; i++)
+            {
+                d.AddLast(buf[i]);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            foreach (object dd in d)
+            {
+                Console.WriteLine(dd);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("Введите значения для поиска.");
+            long cc = long.Parse(Console.ReadLine());
+            if (d.Contains(cc))
+            {
+                Console.WriteLine("Элемент найден. ");
+            }
+            else { Console.WriteLine("Элемент не найден. "); }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+
+
+            //Задание 3
+            ///////////////////////////////////////////////////////////////////////////////
+            HashSet<Товар> okr = new HashSet<Товар>();
+            ///////////////////////////////////////////////////////////////////////////////
+            Товар C = new Мебель();
+            C.InputInformation();
+            C.FindingPrice();
+            C.ShowInformation();
+            Console.WriteLine();
+            okr.Add(C);
+            Товар B = new Диван();
+            B.InputInformation();
+            B.FindingPrice();
+            B.ShowInformation();
+            Console.WriteLine();
+            okr.Add(B);
+            foreach (object z in okr)
+            {
+                Console.WriteLine(z);
+            }
+            ///////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("Удален элемент B :");
+            okr.Remove(B);
+            foreach (object z in okr)
+            {
+                Console.WriteLine(z);
+            }
+            ///////////////////////////////////////////////////////////////////////////////
+            Товар prm1 = new Мебель();
+            prm1.InputInformation();
+            prm1.FindingPrice();
+            prm1.ShowInformation();
+            Console.WriteLine();
+            okr.Add(prm1);
+            foreach (object z in okr)
+            {
+                Console.WriteLine(z);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            LinkedList<Товар> ds = new LinkedList<Товар>();
+            Товар[] buyf = new Товар[size];
+            okr.CopyTo(buyf);
+            ds.AddFirst(buyf[0]);
+            for (int i = 1; i < size; i++)
+            {
+                ds.AddLast(buyf[i]);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("HashSet: ");
+            foreach (object dv in ds)
+            {
+                Console.WriteLine(dv);
+            }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+            if (ds.Contains(C))
+            {
+                Console.WriteLine("Элемент C найден. ");
+            }
+            else { Console.WriteLine("Элемент не найден. "); }
+            Console.WriteLine();
+            ///////////////////////////////////////////////////////////////////////////////
+
+            //Задание 4
+            ///////////////////////////////////////////////////////////////////////////////
+            ObservableCollection<Товар> OBQ = new ObservableCollection<Товар>();
+            obr ad = new obr();
+            OBQ.CollectionChanged += ad.sobposleadd;
+            OBQ.Add(prm1);
+            OBQ.Add(C);
+            foreach (object bb in OBQ)
+            {
+                Console.WriteLine(bb);
+            }
+            OBQ.Remove(prm1);
+            foreach (object bb in OBQ)
+            {
+                Console.WriteLine(bb);
+            }
+        }
+        ///////////////////////////////////////////////////////////////////////////////
+        class obr
+        {
+            public void sobposleadd(object sender, EventArgs e)
+            {
+                Console.WriteLine("Метод, зарегестрированный на событие CollectionChanged");
+            }
+        }
+    
+    }
+}
